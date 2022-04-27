6153998c6c22334a79ef443ecf4eea7f220c08b6
Harsh Rohila <harsh.rohila@mrisoftware.com>
2022-04-27 17:04:12 +0530

    SP-1: added tests for login component

--- src/components/app-login/test/app-login.spec.tsx
+++ src/components/app-login/test/app-login.spec.tsx
@@ -2,17 +2,30 @@ import { newSpecPage } from '@stencil/core/testing';
 import { AppLogin } from '../app-login';
 
 describe('app-login', () => {
-  it('renders', async () => {
+  it('renders form with all fields and submit button', async () => {
+    // act
     const page = await newSpecPage({
       components: [AppLogin],
       html: `<app-login></app-login>`,
     });
-    expect(page.root).toEqualHtml(`
-      <app-login>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </app-login>
-    `);
+
+    // assert
+    const emailField = page.root.shadowRoot.querySelector('input[name="user"]')
+    const passwordField = page.root.shadowRoot.querySelector('input[name="password"]')
+    const submitButton = page.root.shadowRoot.querySelector('input[type="submit"]')
+    expect(emailField).toBeTruthy()
+    expect(passwordField).toBeTruthy()
+    expect(submitButton).toBeTruthy()
   });
+
+  describe('submit button click', () => {
+    it('shows loading', async () => {
+      expect(false).toBeTruthy()
+    });
+  
+    it('shows alert with welcome message for username', async () => {
+      expect(false).toBeTruthy()
+    });
+  })
+ 
 });
