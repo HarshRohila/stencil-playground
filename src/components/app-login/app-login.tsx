444bbfddc4d613e7ed565d45628dce3e00196846
Saif Ullah <saif.ullah@mrisoftware.com>
2022-05-11 18:17:56 +0530

    SP-3: loggedIn component(updated)

--- src/components/app-login/app-login.tsx
+++ src/components/app-login/app-login.tsx
@@ -1,28 +1,61 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Prop, State } from '@stencil/core';
 import { MatchResults } from '@stencil/router';
 
 @Component({
   tag: 'app-login',
-  styleUrl: 'app-login.scss',
+  styleUrls: ['app-login.scss'],
+  assetsDirs: ['../../assets'],
   shadow: true,
 })
 export class AppLogin {
   @Prop() match: MatchResults;
-  @Prop() loading = false;
+
+  @State() warningMessage = false;
+
+  @State() notValid = false;
+
+  @State() loading = false;
+
+  @State() showPassword = false;
 
   data: any = {};
+
+  handlePassword = () => {
+    console.log('handlePassword called');
+    this.showPassword = !this.showPassword;
+  };
 
   handleChange(event: any): any {
     const { name, value } = event.target;
     this.data = { ...this.data, [name]: value };
+    let pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    if (pattern.test(event.target.value) || event.target.value === '') {
+      this.notValid = false;
+    } else {
+      this.notValid = true;
+    }
+    console.log(this.notValid);
+    this.warningMessage = false;
   }
 
-  async handleSubmit(event) {
+  async handleSubmit(event: any) {
     event.preventDefault();
+
+    await this.onSubmit();
+  }
+
+  private async onSubmit() {
     console.log('called', this.data);
+    let pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+
+    if (!pattern.test(this.data.user)) {
+      this.warningMessage = true;
+      // console.log(this.data.user);
+      return;
+    }
 
     this.loading = true;
-    await new Promise((resolve) => {
+    await new Promise(resolve => {
       setTimeout(resolve, 2000);
     });
     alert(` "Welcome ${this.data.user.substring(0, this.data.user.length - 10)}" username is from the email ${this.data.user}`);
@@ -37,16 +70,37 @@ export class AppLogin {
         {this.loading ? (
           <h2 class="loading">loading</h2>
         ) : (
-          <form class="login-form" onSubmit={e => this.handleSubmit(e)}>
+          <form class="login-form" onSubmit={e => this.handleSubmit(e)} novalidate>
             <div class="login-container">
-              <div class="field-wrap">
-                <input type="email" name="user" value={this.data.user} onChange={e => this.handleChange(e)} required />
-              </div>
-              <div class="field-wrap">
-                <input type="password" name="password" value={this.data.password} onChange={e => this.handleChange(e)} required />
-              </div>
+              <div class={`field-wrap${this.notValid ? '-notValid' : ''}`}>
+                <label>Email</label>
+                <br />
 
-              <input type="submit" value="submit" />
+                <input type="email" name="user" value={this.data.user} onInput={e => this.handleChange(e)} required placeholder="Enter Your Email" />
+                {this.warningMessage ? (
+                  <div class="alert">
+                    <i class="fas fa-exclamation-triangle"></i>
+                    <span class="alert-content">Please enter valid email address</span>
+                  </div>
+                ) : null}
+              </div>
+              <div class="field-wrap">
+                <label>Password</label>
+                <br />
+                <input
+                  type={this.showPassword ? 'text' : 'password'}
+                  name="password"
+                  value={this.data.password}
+                  onChange={e => this.handleChange(e)}
+                  required
+                  placeholder="Enter Your Password"
+                />
+
+                <i class="fa fa-eye-slash eye" aria-hidden="true" onClick={this.handlePassword}></i>
+              </div>
+              <div class="btn">
+                <input type="submit" value="Sign In" />
+              </div>
             </div>
           </form>
         )}
